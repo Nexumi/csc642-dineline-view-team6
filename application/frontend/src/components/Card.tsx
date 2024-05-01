75e5d343057b89ee9d7439f772a05042f3ef5eff
Jimmy Pan <jimmypan13570@gmail.com>
2024-05-01 00:53:12 -0700

    WIP i button

--- application/frontend/src/components/Card.tsx
+++ application/frontend/src/components/Card.tsx
@@ -30,7 +30,10 @@ export default function Card(props: {
             viewBox="0 0 24 24"
             stroke-width="1.5"
             stroke="currentColor"
-            class="w-6 h-6"
+            class="w-6 h-6 cursor-pointer"
+            onClick={() => {
+              toast.error("WIP");
+            }}
           >
             <path
               stroke-linecap="round"
